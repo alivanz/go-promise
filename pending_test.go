1d5f07acf8a5b7f172e6deaaeb30379669712aae
alivanz <alivan1627@gmail.com>
2022-06-29 22:58:22 +0700

    add race test

--- pending_test.go
+++ pending_test.go
@@ -1,6 +1,10 @@
 package promise
 
-import "testing"
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+)
 
 func TestPendingThenFirst(t *testing.T) {
 	var u Pending[int64]
@@ -21,6 +25,35 @@ func TestPendingResolveFirst(t *testing.T) {
 	u.Then(func(arg1 int64) {
 		count += 1
 	})
+	if count != 1 {
+		t.Fail()
+	}
+}
+
+func TestPendingRace(t *testing.T) {
+	var u Pending[int64]
+	var wg sync.WaitGroup
+	var count int32
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		u.Then(func(arg1 int64) {
+			atomic.AddInt32(&count, 1)
+			if arg1 != 10 {
+				t.Fail()
+			}
+		})
+		u.Then(func(arg1 int64) {
+			t.Fail()
+		})
+	}()
+	go func() {
+		defer wg.Done()
+		u.Resolve(10)
+		u.Resolve(11)
+		u.Resolve(12)
+	}()
+	wg.Wait()
 	if count != 1 {
 		t.Fail()
 	}
